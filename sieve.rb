b55c45da8ce3c63f30d314783c851beed2d89826
awstahl <awstahl@users.noreply.github.com>
2017-01-04 22:40:23 -0800

    Ah, whitespace

--- sieve.rb
+++ sieve.rb
@@ -10,14 +10,14 @@ module Eratosthenes
       2.upto Math.sqrt( candidates.size ).ceil do |n|
         if candidates[n]
 
-          (2 * n..candidates.size - 1).step n do |f|
+          ( 2 * n..candidates.size - 1 ).step n do |f|
             candidates[f] = false 
           end
         end
       end
 
       primes = []
-      candidates.each_index {|i| (primes << i) if candidates[i]}
+      candidates.each_index {|i| ( primes << i ) if candidates[i]}
       primes
 
     end
